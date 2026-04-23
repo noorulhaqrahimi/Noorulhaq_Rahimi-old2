6d973903c2708f79c61a8fd5755f0364896190ba
noorulhaqrahimi <noorulhaqrahimi11@gmail.com>
2026-04-24 00:17:55 +0430

    responsive

--- src/components/home/Button.jsx
+++ src/components/home/Button.jsx
@@ -7,14 +7,14 @@ const Button = function () {
         <Link
           to="/portfolio"
           className="border-[#fda829] justify-center text-[#fda829] text-[clamp(14px,3vw,25px)] 
-         border-[1px] w-[50%] flex justify-self-center items-center
+         border-[1px] w-[48%] flex justify-self-center items-center
          px-8 rounded-full py-3 font-semibold"
         >
           View My Work
         </Link>
         <Link
           to="/contact"
-          className=" w-[50%] shadow-md items-center text-[clamp(14px,3vw,25px)]
+          className=" w-[48%] shadow-md items-center text-[clamp(14px,3vw,25px)]
                justify-center flex justify-self-center bg-[#fda829] py-3 px-8
                rounded-full  text-white font-semibold"
         >
