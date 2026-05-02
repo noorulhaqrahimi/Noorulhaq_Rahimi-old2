befd22c1e6969a4f63828a9f7624840f3a6aba49
noorulhaqrahimi <noorulhaqrahimi11@gmail.com>
2026-05-02 18:55:02 +0430

    slove image address

--- src/components/home/ImgSlider.jsx
+++ src/components/home/ImgSlider.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 export default function ImageSlider() {
   return (
     <div className=" m-7 mt-[35px] lg:w-[650px] lg:absolute lg:bottom-[1px]
     lg:left-[750px] flex justify-self-center items-center overflow-hidden
     rounded-xl shadow-md">
       <img
-        src="/public/images/Noorulhaq.jpg"
+        src="/images/Noorulhaq.jpg"
         alt="noorulhaq rahimi"
         className="w-[clamp(300px,20vw,1300)] sm:w-[600px] md:w-[744px]
         shadow-sm shadow-[0_4px_12px_rgba(253,168,41,0.08)]  transition-opacity"
