393fbfb94ded281f020c49e4779455aa4a69d6ac
Noorulhaq Rahimi <noorulhaqrahimi11@gmail.com>
2026-05-03 16:23:48 -0400

    make not found page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -5,8 +5,8 @@ const NotFound = () => {
   return (
       <>
           <div className="flex flex-col justify-center items-center h-[100vh]">
-              <h1 className='text-[60px] mb-[40px]'>404 - Oops Not Found</h1>
-               <Link className='text-2xl hover:text-3xl hover:text-red-700' to="/">Home</Link> 
+              <h1 className='text-[clamp(16px,8vw,60px)] mb-[40px]'>404 - Oops Not Found</h1>
+               <Link className='text-[clamp(25px,5vw,44px)] text-green-500 font-bold hover:text-3xl hover:text-red-700' to="/">Home</Link> 
       </div>
       </>
   )
